d851474808aebf6b4cd841ca72496170e467357a
Pedro Humberto <pedrohumbertogc@gmail.com>
2021-08-02 08:38:32 -0300

    adicionado função aliquota, falta terminar

--- salario_liquido.py
+++ salario_liquido.py
@@ -1,5 +1,5 @@
 class Calculadora:
-    def salario_liquido (salario):
+    def salario_liquido(salario):
         if salario <= 1100:
             
             sal_liq = salario - (salario * 0.075)
@@ -16,14 +16,28 @@ class Calculadora:
 
         if salario >= 3305.23:
             alq_3 = salario - (salario * 0.14 - 148.71)
+    
+    def aliquota(salario_liq):
+        if salario_liq <= 1100:
+            aliquota = ((salario_liq * 100) / - salario) + 100
+            return aliquota
+
+        if salario_liq >= 1100.01 and salario <=2203.48:
+            aliquota = ((salario_liq * 100) / - salario) + 100
+            return aliquota    
+    
+    def imposto_de_renda(salario):
+        return
 
 
 
 
 salario = float(input("Digite seu salario: "))
 salario_liq = Calculadora.salario_liquido(salario)
+aliquota  = Calculadora.aliquota(salario_liq)
 
-print(f"Seu salario liquido é {salario_liq:0.2f}")
+print(f"Seu salario liquido é {salario_liq:.2f}")
+print(f"A aliquota do INSS é de: {aliquota:.2f}%")
 # print("Seu salario liquido é %0.2f" % salario_liq) modelo antigo
 # print("Seu salario liquido é {:0.2f}".format(salario_liq))
  
