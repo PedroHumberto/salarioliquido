aca6a2a0b74667e969ca0773e714d1bbb5a02cf4
Pedro Humberto <pedrohumbertogc@gmail.com>
2021-08-07 23:35:57 -0300

    correcao retorno salario com desconto VT

--- salario_liquido.py
+++ salario_liquido.py
@@ -12,9 +12,8 @@ print(f"A aliquota do INSS é de: {aliquota:.4f}%")
 print(f'O valor do seu IR é de {Calculadora.imposto_de_renda(salario):.2f}')
 if resp == "s":
     print(f"Seu V.T é de: {Calculadora.vale_transporte(resp, salario)}")
-else:
-    
-    print(f"Seu salario liquido é {salario_liq:.2f}")
+
+print(f"Seu salario liquido é {salario_liq:.2f}")
 
 # print("Seu salario liquido é %0.2f" % salario_liq) modelo antigo
 # print("Seu salario liquido é {:0.2f}".format(salario_liq))
